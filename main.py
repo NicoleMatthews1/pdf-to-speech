3564ccc60ce149e0fe9a59a4d363670389dea22d
NicoleMatthews1 <100863172+NicoleMatthews1@users.noreply.github.com>
2022-07-04 11:51:24 -0500

    Update main.py
    adding comments

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 import shutil
 import fitz # install using: pip install PyMuPDF
 
+# store your sample.pdf in the same folder
+
 with fitz.open("sample.pdf") as pdf_file:
     text = ""
     for page in pdf_file:
@@ -9,7 +11,7 @@ with fitz.open("sample.pdf") as pdf_file:
 
 print(text)
 
-
+# use your own API key here
 API_KEY = "YOUR API KEY"
 tts_endpoint = "http://api.voicerss.org/"
 
@@ -23,7 +25,7 @@ headers = {
     'content-type': 'audio/wav'
 }
 
-
+# save your speech to the same folder
 local_filename = "download.mp3"
 with requests.get(tts_endpoint, params=parameters, stream=True) as r:
     with open(local_filename, 'wb') as f:
